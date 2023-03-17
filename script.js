070a776898f5631a456479999840b4600747538a
Vanessa Mitelman <vanessamitelman@gmail.com>
2023-03-17 13:12:36 +0200

    loading and empty text

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ let toDoList = document.getElementById('todoList');
 let darkMode = document.getElementById('switch');
 let mainEl = document.getElementById('main');
 
+function noTask() {
+  if (toDoList.innerHTML == '') {
+    let emptyTask = document.createElement('div');
+    emptyTask.setAttribute('id', 'empty');
+    emptyTask.textContent = 'Please Add A New Task';
+    toDoList.appendChild(emptyTask);
+  }
+}
+function removeNoTask() {
+  if (document.getElementById('empty')) {
+    document.getElementById('empty').remove();
+  }
+}
+function addLoader() {
+  if (createBtn.classList.contains('loader')) return;
+  createBtn.classList.add('loader');
+}
+function removeLoader() {
+  createBtn.classList.remove('loader');
+}
+
 darkMode.addEventListener('click', () => {
   if (darkMode.checked == true) {
     mainEl.classList.add('dark-mode');
@@ -13,11 +34,13 @@ darkMode.addEventListener('click', () => {
 });
 
 inputTodo.focus();
+noTask();
 
 function addToDo() {
   inputTodo.focus();
   let val = inputTodo.value;
   if (val == '') return;
+  addLoader();
   let toDoItem = document.createElement('li');
   let toDoText = document.createElement('input');
   toDoText.type = 'text';
@@ -32,12 +55,14 @@ function addToDo() {
   toDoEdit.innerHTML = `<i class="fas fa-edit"></i>`;
   toDoSave.innerHTML = `<i class="fas fa-save"></i>`;
   toDoDelete.innerHTML = `<i class='fa fa-trash' aria-hidden='true'></i>`;
+
   toDoEdit.addEventListener('click', () => {
     toDoText.removeAttribute('readonly');
     toDoText.focus();
   });
   toDoDelete.addEventListener('click', () => {
     toDoItem.remove();
+    noTask();
   });
   toDoSave.addEventListener('click', () => {
     readOnly();
@@ -50,13 +75,17 @@ function addToDo() {
   function readOnly() {
     toDoText.setAttribute('readonly', 'readonly');
   }
-  toDoItem.appendChild(toDoText);
-  btnContainer.appendChild(toDoDelete);
-  btnContainer.appendChild(toDoEdit);
-  btnContainer.appendChild(toDoSave);
+  setTimeout(() => {
+    toDoItem.appendChild(toDoText);
+    btnContainer.appendChild(toDoDelete);
+    btnContainer.appendChild(toDoEdit);
+    btnContainer.appendChild(toDoSave);
 
-  toDoItem.appendChild(btnContainer);
-  toDoList.appendChild(toDoItem);
+    toDoItem.appendChild(btnContainer);
+    toDoList.appendChild(toDoItem);
+    removeNoTask();
+    removeLoader();
+  }, 800);
 }
 
 createBtn.addEventListener('click', addToDo);
